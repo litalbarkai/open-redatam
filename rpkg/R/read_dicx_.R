0d45f45eddc6ad88f2fc918a740074718f958c4b
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-09-22 09:23:55 -0400

    DICX parsing almost ready in R

--- rpkg/R/read_dicx_.R
+++ rpkg/R/read_dicx_.R
@@ -1,44 +1,75 @@
+get_var_type <- function(type) {
+  switch(type,
+    "CHR" = "STRING",
+    "DBL" = "REAL",
+    "INT" = "INT16",
+    "LNG" = "INTEGER",
+    "BIN" = "INTEGER",
+    "PCK" = "INTEGER",
+    "NA" = "",
+    ""
+  )
+}
+
 read_dicx_ <- function(filepath) {
-  # Parse the XML file
   dicx <- read_xml(filepath)
 
-  # Initialize an empty list to store the result
   result <- list()
 
-  # Start by extracting the root information
   root <- xml_find_all(dicx, ".//root")
 
   root_name <- xml_text(xml_find_first(root, ".//name"))
-  root_label <- xml_text(xml_find_first(root, ".//label"))
-  root_filepath <- xml_text(xml_find_first(root, ".//filename"))
-  root_tag <- xml_text(xml_find_first(root, ".//tag"))
+  root_pointer <- xml_find_first(root, ".//filename")
 
   result[[root_name]] <- list(
     name = root_name,
-    label = root_label,
-    filepath = root_filepath,
-    tag = root_tag
+    pointer = xml_text(root_pointer)
   )
-  
-  # Loop through each entity
+
+  variable <- xml_find_first(dicx, ".//variable")
+
+  variable_name <- xml_text(xml_find_first(variable, ".//name"))
+  variable_label <- xml_text(xml_find_first(variable, ".//label"))
+  variable_type <- xml_text(xml_find_first(variable, ".//varType"))
+  variable_field_size <- xml_text(xml_find_first(variable, ".//fieldSize"))
+
+  variable_filename <- xml_text(xml_find_first(variable, ".//filename"))
+  variable_filename <- gsub("\\.\\\\", paste0(dirname(filepath), "/"), variable_filename)
+  variable_filename <- gsub("//", "/", variable_filename)
+
+  result[[root_name]][[variable_name]] <- list(
+    name = variable_name,
+    label = variable_label,
+    type = variable_type,
+    fieldSize = variable_field_size,
+    filename = variable_filename
+  )
+
   entities <- xml_find_all(dicx, ".//entity")
 
   for (entity in entities) {
     entity_name <- xml_text(xml_find_first(entity, ".//name"))
     entity_label <- xml_text(xml_find_first(entity, ".//label"))
-    entity_filepath <- xml_text(xml_find_first(entity, ".//filename"))
-    entity_tag <- xml_text(xml_find_first(entity, ".//tag"))
 
-    # Extract variable information
+    entity_filename <- xml_text(xml_find_first(entity, ".//filename"))
+    entity_filename <- gsub("\\.\\\\", paste0(dirname(filepath), "/"), entity_filename)
+    entity_filename <- gsub("//", "/", entity_filename)
+
     variables <- xml_find_all(entity, ".//variable")
     variable_list <- lapply(variables, function(var) {
       var_name <- xml_text(xml_find_first(var, ".//name"))
       var_label <- xml_text(xml_find_first(var, ".//label"))
+      
+      var_type <- xml_find_first(var, ".//varDicChoice")
+      var_type <- xml_text(xml_find_first(var_type, ".//datasetType"))
+      var_type <- get_var_type(var_type)
+      
       field_size <- xml_text(xml_find_first(var, ".//fieldSize"))
-      tag <- xml_text(xml_find_first(var, ".//tag"))
-      var_filepath <- xml_text(xml_find_first(var, ".//filename"))
 
-      # Extract value labels (if available)
+      var_filename <- xml_text(xml_find_first(var, ".//filename"))
+      var_filename <- gsub("\\.\\\\", paste0(dirname(filepath), "/"), var_filename)
+      var_filename <- gsub("//", "/", var_filename)
+
       value_labels <- xml_find_all(var, ".//valueLabelElement")
       value_label_list <- lapply(value_labels, function(value_label) {
         value <- xml_text(xml_find_first(value_label, ".//value"))
@@ -46,22 +77,26 @@ read_dicx_ <- function(filepath) {
         list(value = value, label = label)
       })
 
-      list(
+      res <- list(
         name = var_name,
         label = var_label,
-        fieldSize = field_size,
-        tag = tag,
-        filepath = var_filepath,
-        valueLabels = value_label_list
+        type = var_type,
+        fieldsize = field_size,
+        filename = var_filename,
+        valuelabels = value_label_list
       )
+
+      # names(res) <- res$name
+
+      return(res)
     })
 
-    # Store the entity and its variables
-    result[[entity_name]] <- list(
+    names(variable_list) <- sapply(variable_list, function(x) x[[1]])
+
+    result[[root_name]][[variable_name]][[entity_name]] <- list(
       name = entity_name,
       label = entity_label,
-      filepath = entity_filepath,
-      tag = entity_tag,
+      filename = entity_filename,
       variables = variable_list
     )
   }
--- rpkg/src/read_dic.cpp
+++ rpkg/src/read_dic.cpp
@@ -4,23 +4,56 @@
 
 #include "Entity.hpp"
 #include "FuzzyEntityParser.hpp"
+#include "Variable.hpp"
 
 using namespace cpp11;
 
-// Helper function to convert variable details into an R-friendly list format
+// taken from XMLExporter.cpp in the original C++ code
+std::string GetVarType(RedatamLib::VarType type) {
+  switch (type) {
+    case RedatamLib::CHR:
+      return "STRING";
+    case RedatamLib::DBL:
+      return "REAL";
+    case RedatamLib::INT:
+      return "INT16";
+    case RedatamLib::LNG:
+    case RedatamLib::BIN:
+    case RedatamLib::PCK:
+      return "INTEGER";
+    case RedatamLib::NA:
+    default:
+      return "";
+  }
+}
+
 list convert_variable_to_list(const RedatamLib::Variable& variable) {
   writable::list var_list;
-  var_list.push_back(cpp11::r_string(variable.GetName()));  // Variable name
-  var_list.push_back(
-      cpp11::r_string(variable.GetFilePath()));  // Variable file path
+  var_list.push_back(cpp11::r_string(variable.GetName()));
+  var_list.push_back(cpp11::r_string(variable.GetDescription()));
+  var_list.push_back(cpp11::r_string(GetVarType(variable.GetType())));
+  var_list.push_back(integers({static_cast<int>(variable.GetDataSize())}));
+
+  // writable::list tag_list;
+  // for (const auto& tag : variable.GetTags()) {
+  //   writable::list tag_entry;
+  //   tag_entry.push_back(cpp11::r_string(tag.first));   // Value
+  //   tag_entry.push_back(cpp11::r_string(tag.second));  // Label
+  //   tag_list.push_back(tag_entry);
+  // }
+  // var_list.push_back(tag_list);
+
+  var_list.push_back(cpp11::r_string(variable.GetFilePath()));
 
   // Set names for the list elements
-  var_list.names() = {"name", "filepath"};
+  // var_list.names() = {"name", "label", "type", "fieldSize", "tag", 
+  //                     "filename", "valueLabels"};
+  var_list.names() = {"name", "label", "type", "fieldSize", "filename",
+                      "valueLabels"};
 
   return var_list;
 }
 
-// Helper function to convert entities into an R-friendly list format
 list convert_entities_to_list(
     const std::vector<RedatamLib::Entity>& entities) {
   writable::list result;
@@ -28,43 +61,37 @@ list convert_entities_to_list(
 
   for (const auto& entity : entities) {
     writable::list entity_list;
-    entity_list.push_back(cpp11::r_string(entity.GetName()));  // Entity name
+    entity_list.push_back(cpp11::r_string(entity.GetName()));
+    entity_list.push_back(cpp11::r_string(entity.GetPTRPath()));
 
     // Dereference the shared_ptr to access the vector of variables
-    const auto& variables =
-        *(entity.GetVariables());  // Dereference the shared_ptr
+    const auto& variables = *(entity.GetVariables());
 
     // Convert each variable within the entity
     writable::list variable_list;
     for (const auto& variable : variables) {
-      variable_list.push_back(
-          convert_variable_to_list(variable));  // Convert variable
+      variable_list.push_back(convert_variable_to_list(variable));
     }
 
     // Set names for the entity list elements
-    entity_list.push_back(variable_list);  // Add variable list to the entity
-    entity_list.names() = {"name", "variables"};  // Name the entity elements
+    entity_list.push_back(variable_list);
+    entity_list.names() = {"name", "filename", "variables"};
 
-    result.push_back(entity_list);  // Add the entity to the result
-    entity_names.push_back(cpp11::r_string(
-        entity.GetName()));  // Collect the entity name for list naming
+    result.push_back(entity_list);
+    entity_names.push_back(cpp11::r_string(entity.GetName()));
   }
 
-  result.names() = entity_names;  // Assign entity names as names for the list
+  result.names() = entity_names;
 
   return result;
 }
 
-// Efficient function to read and parse a DIC file using FuzzyEntityParser
 [[cpp11::register]] list read_dic_(const std::string& filepath) {
   try {
-    // Initialize the FuzzyEntityParser to parse the DIC file
     RedatamLib::FuzzyEntityParser parser(filepath);
 
-    // Parse the entities from the DIC file
     std::vector<RedatamLib::Entity> entities = parser.ParseEntities();
 
-    // Convert the parsed entities into an R list and return it
     return convert_entities_to_list(entities);
 
   } catch (const std::exception& e) {

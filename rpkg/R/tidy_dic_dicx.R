cb954c3b07e6cd12b7fba14c904f889e9f2e6b88
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-09-23 11:38:54 -0400

    tidy lists on R side

--- rpkg/R/tidy_dic_dicx.R
+++ rpkg/R/tidy_dic_dicx.R
@@ -8,20 +8,46 @@ fix_encoding_ <- function(x) {
   }
 }
 
-discard_empty_ <- function(x) {
+replace_empty_with_na_ <- function(x) {
   if (is.list(x)) {
-    x <- lapply(x, discard_empty)
-    x <- x[!sapply(x, function(y) is.null(y) || (is.list(y) && length(y) == 0)
-      || (is.character(y) && length(y) == 0))]
+    x <- lapply(x, replace_empty_with_na_) # Recursively apply to sublists
+
+    x <- lapply(x, function(y) {
+      if (is.list(y) && length(y) == 0L) {
+        return(NA)
+      } else {
+        return(y)
+      }
+    })
+  } else if (is.character(x) && isTRUE(nchar(x) == 0L)) {
+    return(NA)
+  } else if (is.null(x)) {
+    return(NA)
   }
+
   return(x)
+}
+
+remove_name_ <- function(dic) {
+  dic <- lapply(dic, function(x) {
+    if (is.list(x)) {
+      # Recursively apply to sublists
+      x <- remove_name_(x)
+      # Remove elements with the name "name"
+      if (!is.null(names(x))) {
+        x <- x[names(x) != "name"]
+      }
+    }
+    return(x)
+  })
+  return(dic)
 }
 
 tidy_dic_dicx_ <- function(dictionary) {
   # 1. fix encoding (e.g., ""C\xf3digo Region" -> "Código Region")
   # 2. if an element is an empty list, remove it
   dic <- fix_encoding_(
-    discard_empty_(
+    replace_empty_with_na_(
       if (grepl("\\.dic$|\\.DIC$", dictionary)) {
         read_dic_(dictionary)
       } else if (grepl("\\.dicx$|\\.DICX$||\\.dicX", dictionary)) {
@@ -31,6 +57,9 @@ tidy_dic_dicx_ <- function(dictionary) {
       }
     )
   )
+
+  # 3. remove list[["name"]] because the name is already in the list name
+  dic <- remove_name_(dic)
 
   return(dic)
 }

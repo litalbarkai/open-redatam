2d656c5061d728f4088cc30341ca793316ff849d
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-10-09 21:02:16 -0400

    separate mac and ubuntu builds

--- install/mac.sh
+++ install/mac.sh
@@ -12,15 +12,15 @@ unzip /tmp/redatam/redatam.zip -d /tmp/redatam
 # Install the binaries
 sudo mkdir -p /usr/local/bin
 sudo cp /tmp/redatam/redatam /usr/local/bin/
-sudo cp /tmp/redatam/redatamgui.app /usr/local/bin/
+sudo cp /tmp/redatam/redatamgui /usr/local/bin/
 sudo chmod +x /usr/local/bin/redatam
-sudo chmod +x /usr/local/bin/redatamgui.app
+sudo chmod +x /usr/local/bin/redatamgui
 
 # Create a desktop entry for Redatam GUI
 echo "[Desktop Entry]
 Version=1.0
 Name=Redatam GUI
-Exec=/usr/local/bin/redatamgui.app
+Exec=/usr/local/bin/redatamgui
 Icon=utilities-terminal
 Type=Application
 Categories=Utility;" | sudo tee /usr/share/applications/redatamgui.desktop
